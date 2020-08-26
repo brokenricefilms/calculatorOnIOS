6fdba0ca4f2555140122eeb5ea60470d1195ba99
thuan <phamtanthuan2311@gmail.com>
2020-08-26 16:01:43 +0700

    touched...

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("button");
 document.body.appendChild(button);
-const paragraph = document.querySelector(".output");
 
-input.addEventListener("click", function (input) {
+const paragraph = document.querySelector(".output");
+addEventListener("click", function (input) {
   paragraph.innerText = input.innerText;
 });
