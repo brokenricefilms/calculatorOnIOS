acc6ba4f611dd187974667de301ab24961d7ada0
thuan <phamtanthuan2311@gmail.com>
2020-08-29 20:56:56 +0700

    happy monday _ bleh _

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ https://btholt.github.io/intro-to-web-dev-v2/js-project/
 
 ## Review
 
-<img scr="./img/calculatorOnIOSPreview.png"/>
+<img src="./img/calculatorOnIOSPreview.png"/>
--- script.js
+++ script.js
@@ -87,9 +87,11 @@ function rerender() {
 }
 
 function init() {
-  document.querySelector(".button-container").addEventListener("click", function(event) {
-    buttonClick(event.target.innerText);
-  });
+  document
+    .querySelector(".button-container")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
 }
 
 init();
